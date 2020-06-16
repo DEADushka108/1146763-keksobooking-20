f4f0c1f089a14e1b7079c59d1234a46a63d627e3
DEADushka108 <cloxx108@gmail.com>
2020-06-16 15:02:46 +0500

    Добавляет небольшие улучшения

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 'use strict';
 (function () {
-  var TOTAL = 8;
+  var TOTAL = 5;
+  var DATA_URL = 'https://javascript.pages.academy/keksobooking';
   var MAIN_PIN_DEAFULT_TOP = '375px';
   var MAIN_PIN_DEAFULT_LEFT = '570px';
+
+  var getData = window.data.get;
+  var toggleFilters = window.filter.toggle;
 
   var map = document.querySelector('.map');
   var mainPin = document.querySelector('.map__pin--main');
   var form = window.form.element;
-  var leaseAds = window.mock.createAds(TOTAL);
+  // var leaseAds = window.mock.createAds(TOTAL);
   var resetButton = form.querySelector('.ad-form__reset');
+
+  window.leaseAds = [];
+
+  function onError(message) {
+    var errorTemplate = document.querySelector('#error').textContent.querySelector('.error');
+    var errorElement = errorTemplate.cloneNode(true);
+    errorElement.querySelector('.error__masage').textContent = message;
+    errorElement.querySelector('.error__button').addEventListener('click', function () {
+      errorElement.remove();
+      getData(DATA_URL, onSuccess, onError);
+    });
+  }
+
+  function onSuccess(data) {
+    window.leaseAds = data;
+    window.pin.render(window.leaseAds.slice(0, TOTAL));
+  }
 
   function isMapActive() {
     return !(map.classList.contains('map--faded'));
@@ -21,17 +42,20 @@
   function setPageActive() {
     map.classList.remove('map--faded');
     window.form.toggle();
+    toggleFilters();
   }
 
   function setPageDisactive() {
     map.classList.add('map--faded');
     window.form.toggle();
+    toggleFilters();
   }
 
   function onMainPinMouseupActivatePage() {
     if (!isMapActive() && !isFormActive()) {
       setPageActive();
-      window.pin.render(leaseAds);
+      getData(DATA_URL, onSuccess, onError);
+      mainPin.removeEventListener('mousedown', onMainPinMouseupActivatePage);
     }
   }
 
@@ -43,17 +67,27 @@
     initPage();
   }
 
+  function onKeyPressActivatePage(evt) {
+    if (evt.key === 'Enter') {
+      onMainPinMouseupActivatePage();
+    }
+  }
+
   function initPage() {
     setPageDisactive();
   }
 
   function onResetButtonClick(evt) {
     evt.preventDefault();
+    window.filter.toggle();
     form.reset();
     clearPage();
+    window.leaseAds = [];
+    mainPin.addEventListener('mousedown', onMainPinMouseupActivatePage);
   }
 
   initPage();
+  mainPin.addEventListener('keydown', onKeyPressActivatePage);
   mainPin.addEventListener('mousedown', window.dragndrop.movePin);
   mainPin.addEventListener('mouseup', onMainPinMouseupActivatePage);
   resetButton.addEventListener('click', onResetButtonClick);
